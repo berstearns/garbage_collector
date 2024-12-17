3b4368b8f9bd85083d56b12c04e6e4939c4182d5
bernardo stearns <bernardo.stearns@gmail.com>
2024-12-17 14:46:36 +0000

    adding base_folder as variable

--- webassembly101/ctowasmwasi.sh
+++ webassembly101/ctowasmwasi.sh
@@ -1,3 +1,4 @@
-/opt/wasi-sdk/bin/clang --target=wasm32-wasi -O3 -emit-llvm -c hello.c -o hello.bc --sysroot=/opt/wasi-sdk/share/wasi-sysroot
+BASE_FOLDER=./wasi-sdk-24.0-x86_64-linux
+$BASE_FOLDER/bin/clang --target=wasm32-wasi -O3 -emit-llvm -c hello.c -o hello.bc --sysroot=$BASE_FOLDER/share/wasi-sysroot
 llc -march=wasm32 -filetype=obj -o hello.o hello.bc
-/opt/wasi-sdk/bin/clang  --target=wasm32-wasi -o hello.wasm hello.o -lc -lm -Wl,--export-all -Wl,--no-entry --sysroot=/opt/wasi-sdk/share/wasi-sysroot
+$BASE_FOLDER/bin/clang  --target=wasm32-wasi -o hello.wasm hello.o -lc -lm -Wl,--export-all -Wl,--no-entry --sysroot=$BASE_FOLDER/share/wasi-sysroot
