57bc9946651f95419b2e3f7bae456393e0ae73b6
bernardo stearns <bernardo.stearns@gmail.com>
2024-12-17 17:44:00 +0000

    updates

--- webassembly101/wasmtimenow.py
+++ webassembly101/wasmtimenow.py
@@ -5,7 +5,9 @@ from wasmtime import Store, Module, Instance, Func, FuncType
 import wasmtime.loader
 # Assuming `your_wasm_file.wasm` is in the python load path...
 import hello 
+import fast_align
 
 # Now you're compiled and instantiated and ready to go!
 result = hello.main(1,1)
 print(result)
+fast_align.main()
