64f9d13c5c2ff99c05f29911136825e368899fb6
bernardostearns <you@example.com>
2024-12-29 19:06:50 +0000

    gdrive.sh updates

--- gdrive/gdrive.sh
+++ gdrive/gdrive.sh
@@ -18,21 +18,27 @@ pull() {
 push() {
     echo "Pushing files from $SCRIPT_FOLDERPATH to $REMOTE..."
     if [ "$SHARED" = true ]; then
-        rclone copyto --progress --drive-shared-with-me "$SCRIPT_FOLDERPATH" "$REMOTE:/celso-bernardo/gapster"
+        rclone copyto --progress --drive-shared-with-me --exclude "*.sh" "$SCRIPT_FOLDERPATH" "$REMOTE:/celso-bernardo/gapster"
     else
-        rclone copyto --progress "$SCRIPT_FOLDERPATH" "$REMOTE:/celso-bernardo/gapster"
+        rclone copyto --progress --exclude "*.sh" "$SCRIPT_FOLDERPATH" "$REMOTE:/celso-bernardo/gapster"
+    fi
+
+}
+
+# Main function to handle script logic
+main() {
+    echo "The absolute path of this script is: $SCRIPT_FOLDERPATH"
+
+    # Check for command-line arguments to determine whether to pull or push
+    if [ "$1" = "pull" ]; then
+        pull
+    elif [ "$1" = "push" ]; then
+        push
+    else
+        echo "Usage: $0 {pull|push}"
+        exit 1
     fi
 }
 
-# Main script logic
-echo "The absolute path of this script is: $SCRIPT_FOLDERPATH"
-
-# Check for command-line arguments to determine whether to pull or push
-if [ "$1" = "pull" ]; then
-    pull
-elif [ "$1" = "push" ]; then
-    push
-else
-    echo "Usage: $0 {pull|push}"
-    exit 1
-fi
+# Call the main function and pass all command-line arguments
+main "$@"
